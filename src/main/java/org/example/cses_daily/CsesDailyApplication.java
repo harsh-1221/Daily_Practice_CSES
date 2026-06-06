e4ca18f3a0b080b42cf76cf36979734efc5d3026
harsh-1221 <75370741+harsh-1221@users.noreply.github.com>
2026-06-06 17:47:56 +0530

    init commit

---
+++ src/main/java/org/example/cses_daily/CsesDailyApplication.java
@@ -0,0 +1,13 @@
+package org.example.cses_daily;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsesDailyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CsesDailyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/cses_daily/CsesDailyApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.cses_daily;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsesDailyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
